0bd53825bb732d923ce42700e49c0116521652f0
Taoufiq elhanchaoui <t.elhanchaoui@gmail.com>
2023-04-27 16:28:25 +0100

    change Routing

--- src/components/App/Router.js
+++ src/components/App/Router.js
@@ -49,39 +49,39 @@ function Routing () {
 
      {/* Routes Acceuil  */}
 
-            <Route path="/" exact element={<Acceuil/>} />
-            <Route path="/resultSearchMedcine" exact element={<ResultSearchMedcine/>} />
-            <Route path="/rendezVous/:idMedcine/:login" exact element={<RendezVous/>} />
+            <Route path='/' exact element={<Acceuil/>} />
+            <Route path='/resultSearchMedcine' exact element={<ResultSearchMedcine/>} />
+            <Route path='/rendezVous/:idMedcine/:login' exact element={<RendezVous/>} />
 
     {/* Routes Patient  */}
-            <Route path="/espacePatient" exact element={<EspacePatient/>} />
-            <Route path="/loginPatient" exact element={<LoginPatient/>} />
-            <Route path="/signUpPatient" exact element={<SignUpPatient/>} />
-            <Route path="/patient/activateCompte/:token" exact element={<ValidateAccount/>} /> 
-            <PrivateRoutePatient path="/dashboardPatient" exact element={<DashboardPatient/>} /> 
-            <PrivateRoutePatient path="/searchMedcine" exact element={<SearchMedcine/>} /> 
-            <PrivateRoutePatient path="/myAccount" exact element={<AccountPatient/>} /> 
-            <PrivateRoutePatient path="/updateMyAccount" exact element={<UpdateAccountPatient/>} />
-            <PrivateRoutePatient path="/listOrdonnancesPatient" exact element={<ListOrdonnances/>} />  
+            <Route path='/espacePatient' exact element={<EspacePatient/>} />
+            <Route path='/loginPatient' exact element={<LoginPatient/>} />
+            <Route path='/signUpPatient' exact element={<SignUpPatient/>} />
+            <Route path='/patient/activateCompte/:token' exact element={<ValidateAccount/>} /> 
+            <PrivateRoutePatient path='/dashboardPatient' exact element={<DashboardPatient/>} /> 
+            <PrivateRoutePatient path='/searchMedcine' exact element={<SearchMedcine/>} /> 
+            <PrivateRoutePatient path='/myAccount' exact element={<AccountPatient/>} /> 
+            <PrivateRoutePatient path='/updateMyAccount' exact element={<UpdateAccountPatient/>} />
+            <PrivateRoutePatient path='/listOrdonnancesPatient' exact element={<ListOrdonnances/>} />  
 
     {/* Routes Medecin  */}      
-            <Route path="/espaceMédecin" exact element={<EspaceMedecin/>} />
-            <Route path="/loginMedcine" exact element={<LoginMedcine/>} />
-            <Route path="/signUpMedcine" exact element={<SignUpMedcine/>} />
-            <PrivateRoute path="/createAccountSecretary" exact element={<CreateAccountSecretary/>} />
-            <Route path="/medcine/activateCompte/:token" exact element={<ValidateAccountMedcine/>} /> 
-            <PrivateRoute path="/dashboardMedcine" exact element={<DashboardMedcine/>} />
-            <PrivateRoute path="/secretaryCompte" exact element={<SecretaryCompte/>} />
-            <PrivateRoute path="/managementCompteSecretary" exact element={<ManagementCompteSecretary/>} />
-            <PrivateRoute path="/managementAvailablityMedcine" exact element={<ManagementAvailablityMedcine/>} />
-            <PrivateRoute path="/listAppointments" exact element={<ListAppointments/>} />
-            <PrivateRoute path="/createOrdonnance" exact element={<CreateOrdonnances/>} />
-            <PrivateRoute path="/listOrdonnances" exact element={<Ordonnances/>} />
+            <Route path='/espaceMédecin' exact element={<EspaceMedecin/>} />
+            <Route path='/loginMedcine' exact element={<LoginMedcine/>} />
+            <Route path='/signUpMedcine' exact element={<SignUpMedcine/>} />
+            <PrivateRoute path='/createAccountSecretary' exact element={<CreateAccountSecretary/>} />
+            <Route path='/medcine/activateCompte/:token' exact element={<ValidateAccountMedcine/>} /> 
+            <PrivateRoute path='/dashboardMedcine' exact element={<DashboardMedcine/>} />
+            <PrivateRoute path='/secretaryCompte' exact element={<SecretaryCompte/>} />
+            <PrivateRoute path='/managementCompteSecretary' exact element={<ManagementCompteSecretary/>} />
+            <PrivateRoute path='/managementAvailablityMedcine' exact element={<ManagementAvailablityMedcine/>} />
+            <PrivateRoute path='/listAppointments' exact element={<ListAppointments/>} />
+            <PrivateRoute path='/createOrdonnance' exact element={<CreateOrdonnances/>} />
+            <PrivateRoute path='/listOrdonnances' exact element={<Ordonnances/>} />
     {/* Routes Secretary  */}      
-            <Route path="/loginSecretary" exact element={<LoginSecretary/>} />
-            <PrivateRouteSecretary path="/dashboardSecretary" exact element={<DashboardSecretary/>} />
-            <PrivateRouteSecretary path="/confirmAppointment" exact element={<ConfirmAppointment/>} />
-            <PrivateRouteSecretary path="/alertAppointment" exact element={<AlertAppointment/>} />
+            <Route path='/loginSecretary' exact element={<LoginSecretary/>} />
+            <PrivateRouteSecretary path='/dashboardSecretary' exact element={<DashboardSecretary/>} />
+            <PrivateRouteSecretary path='/confirmAppointment' exact element={<ConfirmAppointment/>} />
+            <PrivateRouteSecretary path='/alertAppointment' exact element={<AlertAppointment/>} />
             
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routes from './elements/App/Router'
+import Routes from './components/App/Router'
 import './fontawesome';
 
 ReactDOM.render(
